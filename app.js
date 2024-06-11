f9c0b2e3f553a836d879130182d2922599e28934
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-06-11 07:53:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+/* Created by Tivotal */
+
+let input = document.querySelector("input");
+let text = document.querySelector(".text");
+
+input.addEventListener("input", () => {
+  let inputValue = input.value;
+  inputValue = inputValue.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+  let pattern = new RegExp(`${inputValue}`, "gi");
+
+  text.innerHTML = text.textContent.replace(
+    pattern,
+    (match) => `<mark style="background:#ffdd4b">${match}</mark>`
+  );
+});
